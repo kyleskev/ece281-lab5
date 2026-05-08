1f278fe5b53845907f080f673182bc0d35150706
kyleskev <c28kyle.skevington@afacademy.af.edu>
2026-05-08 11:10:04 -0600

    lab5

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -40,8 +40,28 @@ entity ALU is
 end ALU;
 
 architecture Behavioral of ALU is
-
+    signal c_result : std_logic_vector(7 downto 0);
 begin
+    process(i_A, i_B, i_op)
+    begin
+        case i_op is
+            when "000" => --ADD
+                c_result <= std_logic_vector(unsigned(i_A) + unsigned(i_B));
+            when "001" => --SUBTRACT
+                c_result <= std_logic_vector(unsigned(i_A) - unsigned(i_B));
+            when "010" =>
+                c_result <= i_A and i_B;
+            when "011" =>
+                c_result <= i_A or i_B;
+            when others => 
+                c_result <= "00000000";
+       end case;
+   end process;
 
-
+    o_result <= c_result;
+    
+    o_flags(3) <= c_result(7);
+    o_flags(2) <= '1' when c_result = "00000000" else '0';
+    o_flags(1) <= '0';
+    o_flags(0) <= '0';
 end Behavioral;
--- src/controller_fsm.vhd
+++ src/controller_fsm.vhd
@@ -38,8 +38,40 @@ entity controller_fsm is
 end controller_fsm;
 
 architecture FSM of controller_fsm is
-
+    type state_type is (s_load_a, s_load_b, s_execute, s_display);
+    signal current_state : state_type := s_load_a;
 begin
+    process(i_adv, i_reset)
+    begin
+        if i_reset = '1' then
+            current_state <= s_load_a;
+        elsif rising_edge(i_adv) then
+            case current_state is
+                when s_load_a =>
+                    current_state <= s_load_b;
+                when s_load_b => 
+                    current_state <= s_execute;
+                when s_execute =>
+                    current_state <= s_display;
+                when s_display => 
+                    current_state <= s_load_a;
+            end case; 
+         end if;
+     end process;
+     
+     process(current_state)
+     begin
+        case current_state is
+            when s_load_a =>
+                o_cycle <= "0001";
+            when s_load_b =>
+                o_cycle <= "0010";
+            when s_execute => 
+                o_cycle <= "0100";
+            when s_display =>
+                o_cycle <= "1000";
+        end case;
+    end process;
 
 
 end FSM;
