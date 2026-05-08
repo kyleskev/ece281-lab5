b19596416a6796faf1ac38f9da126019a2f1cd81
kyleskev <c28kyle.skevington@afacademy.af.edu>
2026-05-08 11:13:11 -0600

    lab555

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -40,28 +40,78 @@ entity ALU is
 end ALU;
 
 architecture Behavioral of ALU is
-    signal c_result : std_logic_vector(7 downto 0);
+    signal c_result9 : unsigned(8 downto 0);
+    signal c_result  : std_logic_vector(7 downto 0);
 begin
+
     process(i_A, i_B, i_op)
+        variable v_a_signed : signed(7 downto 0);
+        variable v_b_signed : signed(7 downto 0);
+        variable v_r_signed : signed(7 downto 0);
     begin
+        c_result9 <= (others => '0');
+        c_result  <= (others => '0');
+        o_flags   <= (others => '0');
+
+        v_a_signed := signed(i_A);
+        v_b_signed := signed(i_B);
+
         case i_op is
-            when "000" => --ADD
-                c_result <= std_logic_vector(unsigned(i_A) + unsigned(i_B));
-            when "001" => --SUBTRACT
-                c_result <= std_logic_vector(unsigned(i_A) - unsigned(i_B));
-            when "010" =>
+            when "000" => -- ADD
+                c_result9 <= ('0' & unsigned(i_A)) + ('0' & unsigned(i_B));
+                c_result  <= std_logic_vector(unsigned(i_A) + unsigned(i_B));
+
+            when "001" => -- SUBTRACT
+                c_result9 <= ('0' & unsigned(i_A)) - ('0' & unsigned(i_B));
+                c_result  <= std_logic_vector(unsigned(i_A) - unsigned(i_B));
+
+            when "010" => -- AND
                 c_result <= i_A and i_B;
-            when "011" =>
+
+            when "011" => -- OR
                 c_result <= i_A or i_B;
-            when others => 
-                c_result <= "00000000";
-       end case;
-   end process;
+
+            when others =>
+                c_result <= (others => '0');
+        end case;
+
+        v_r_signed := signed(c_result);
+
+        -- N flag
+        o_flags(3) <= c_result(7);
+
+        -- Z flag
+        if c_result = "00000000" then
+            o_flags(2) <= '1';
+        else
+            o_flags(2) <= '0';
+        end if;
+
+        -- C flag
+        if i_op = "000" or i_op = "001" then
+            o_flags(1) <= c_result9(8);
+        else
+            o_flags(1) <= '0';
+        end if;
+
+        -- V flag
+        if i_op = "000" then
+            if (i_A(7) = i_B(7)) and (c_result(7) /= i_A(7)) then
+                o_flags(0) <= '1';
+            else
+                o_flags(0) <= '0';
+            end if;
+        elsif i_op = "001" then
+            if (i_A(7) /= i_B(7)) and (c_result(7) /= i_A(7)) then
+                o_flags(0) <= '1';
+            else
+                o_flags(0) <= '0';
+            end if;
+        else
+            o_flags(0) <= '0';
+        end if;
+    end process;
 
     o_result <= c_result;
-    
-    o_flags(3) <= c_result(7);
-    o_flags(2) <= '1' when c_result = "00000000" else '0';
-    o_flags(1) <= '0';
-    o_flags(0) <= '0';
+
 end Behavioral;
